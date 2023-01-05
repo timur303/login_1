aec2ca3fc4328d246a1457209c41112f27c5b02c
Тимур <kadyrbekovtimur001@gmail.com>
2023-01-05 15:48:25 +0600

    Initial commit

---
+++ src/main/java/com/example/Login1Application.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Login1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Login1Application.class, args);
+    }
+
+}
